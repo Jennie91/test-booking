e9b35d59eeaeee3b5311e30c2169ed13af9a6265
Jennie <jennie_0114@hotmail.com>
2015-09-16 14:03:26 +0200

    initial-commit

---
+++ room-boking/room-boking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(room_boking.Startup))]
+namespace room_boking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
